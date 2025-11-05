bb5c01472daabe9fb352bee13867bfec1f244b4f
sonal1809 <sonalkumar181997@gmail.com>
2025-11-05 09:48:16 +0530

    Initial commit - Spring Boot project setup

---
+++ src/main/java/org/example/userservice25/UserService25Application.java
@@ -0,0 +1,13 @@
+package org.example.userservice25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserService25Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserService25Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/userservice25/UserService25ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.userservice25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserService25ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
